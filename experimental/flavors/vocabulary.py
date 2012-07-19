068e94e9452020e7aff7273ce2df51a7dc3c41ff
Sean Upton <sdupton@gmail.com>
2012-07-19 14:55:11 -0600

    filter FTI behaviors out of the list of available flavors, if it is a given, don't make it selectable or removable.

--- experimental/flavors/vocabulary.py
+++ experimental/flavors/vocabulary.py
@@ -4,9 +4,15 @@ from zope.component import getUtilitiesFor
 from zope.schema import getFieldNamesInOrder
 from zope.schema.interfaces import IContextSourceBinder
 from zope.schema.vocabulary import SimpleVocabulary, SimpleTerm
+from Products.CMFCore.utils import getToolByName
 
 import interfaces
 
+
+def fti_behaviors(context):
+    tool = getToolByName(context, 'portal_types')
+    return tool.get(context.portal_type).behaviors
+    
 
 @grok.provider(IContextSourceBinder)
 def available_flavors(context):
@@ -19,12 +25,14 @@ def available_flavors(context):
     """
     enumerated = getUtilitiesFor(interfaces.IFlavor)
     context_ifaces = set(providedBy(context).flattened())
+    _type_behaviors = fti_behaviors(context)
     _term = lambda name, title: SimpleTerm(name, title=unicode(title))
     # trivia:   CPython's set intersection is effectively as efficient
     #           regardless of  relative size of first/second set.
     vocab = [
         _term(name, flavor.title or name) for name, flavor in enumerated
-            if context_ifaces.intersection(flavor.interfaces)
+            if (context_ifaces.intersection(flavor.interfaces) and
+                name not in _type_behaviors)
         ]
     return SimpleVocabulary(vocab)
 
